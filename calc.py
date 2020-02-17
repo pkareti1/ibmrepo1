05d02a54cb70b9207862bdd62efaad2afd3982d8
root <root@ip-172-31-1-46.us-east-2.compute.internal>
2020-02-17 07:39:19 +0000

    2nd Commit

---
+++ calc.py
@@ -0,0 +1,15 @@
+def add(x, y):
+    """Add Function"""
+    return x+y
+
+def substract(x,y):
+    """Subtract Function"""
+    return x-y
+
+def multiply(x,y):
+    """Multiply Function"""
+    return x*y
+
+def divide(x,y):
+    """Divide Function"""
+    return x/y
